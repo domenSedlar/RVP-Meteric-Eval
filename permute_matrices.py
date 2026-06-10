f7b104a36398a1aed2bf32b4f01dfe27944aa969
domenSedlar <domen.sedlar@gmail.com>
2026-06-10 18:29:59 +0200

    Added support for evaluating GDS heatmaps (I dropped their id columns)

--- permute_matrices.py
+++ permute_matrices.py
@@ -21,26 +21,59 @@ def eval_matrix(A, permutations1, permutations2):
 
 def full_eval(A):
     (m,n) = A.shape
-    p1 = list(itertools.permutations(range(m)))
-    p2 = list(itertools.permutations(range(n)))
-    return eval_matrix(A, p1, p2)
+    permutations1 = list(itertools.permutations(range(m)))
+    permutations2 = list(itertools.permutations(range(n)))
+    results = []
+    total = len(permutations1) * len(permutations2)
+    progress_interval = 100000
+    current = 0
+    for p1 in permutations1:
+        for p2 in permutations2:
+
+            current += 1
+
+            if current % progress_interval == 0:
+                print(f"Progress: {current}/{total} ({current/total*100:.1f}%)")
+
+            a = A[np.ix_(p1, p2)]
+            r = score(a)
+            r['p1'] = tuple(p1)
+            r['p2'] = tuple(p2)
+            results.append(r)
+
+    return results
 
 def random_eval(A):
     (m,n) = A.shape
+    k = 40000
+    permutations = [(random.sample(range(m), m), random.sample(range(n), n)) for _ in range(k)]    
+    
+    results = []
 
-    p1 = [random.shuffle(range(m)) for _ in range(100)]
-    p2 = [random.shuffle(range(n)) for _ in range(100)]
+    for i, (p1, p2) in enumerate(permutations):
+        
+        if i % 100000 == 0:
+            print(f"Progress: {i}/{k} ({i/k*100:.1f}%)")
 
-    return eval_matrix(A)
+        a = A[np.ix_(p1, p2)]
+        r = score(a)
+        r['p1'] = tuple(p1)
+        r['p2'] = tuple(p2)
+        results.append(r)
+
+    return results
 
 def get_results(pth):
-    A = np.loadtxt(pth, delimiter='\t')
+    df = pd.read_csv(pth, delimiter='\t')
+    df = df.drop(columns=['ID_REF', 'IDENTIFIER'], errors='ignore')
+    A = df.to_numpy()
 
     output_file = pth.split('\\')[-1].split("/")[-1].split('.')[0] + '.csv'
 
     (m,n) = A.shape
 
     if max(m,n) < 10:
+        print("doing a full eval")
         r = full_eval(A)
     else:
         r = random_eval(A)
